6c304beaf736cddba4e1e4bf04037ab9ee4c90db
Macros Meng <Meng-yc@outlook.com>
2026-02-14 20:20:55 +0800

    feat: <main>Remessy->ReMessy

--- remessydesktop.py
+++ remessydesktop.py
@@ -2,14 +2,14 @@ from src.util.log import log
 from src.util.versioning import VERSION_FULL
 
 
-class RemessyDesktop:
+class ReMessyDesktop:
     def __init__(self):
-        log.success(f"RemessyDesktop ({VERSION_FULL}) 成功初始化。")
+        log.success(f"ReMessyDesktop ({VERSION_FULL}) 成功初始化。")
 
     def run(self):
-        log.success("RemessyDesktop 开始运行。")
+        log.success("ReMessyDesktop 开始运行。")
 
 
 if __name__ == "__main__":
-    app = RemessyDesktop()
+    app = ReMessyDesktop()
     app.run()
