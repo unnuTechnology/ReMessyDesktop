56147bb99108a4ed1c19765645a2330b73dcb7dc
Macros Meng <Meng-yc@outlook.com>
2026-02-14 21:33:41 +0800

    feat: 配置读取完善，使用pydantic

--- remessydesktop.py
+++ remessydesktop.py
@@ -1,9 +1,12 @@
 from src.util.log import log
 from src.util.versioning import VERSION_FULL
+from src.util import config
 
 
 class ReMessyDesktop:
     def __init__(self):
+        log.info("ReMessyDesktop 正在初始化……")
+        self.config = config.get_config()
         log.success(f"ReMessyDesktop ({VERSION_FULL}) 成功初始化。")
 
     def run(self):
@@ -11,5 +14,9 @@ class ReMessyDesktop:
 
 
 if __name__ == "__main__":
-    app = ReMessyDesktop()
-    app.run()
+    try:
+        app = ReMessyDesktop()
+        app.run()
+    except Exception as e:
+        log.critical(f"发生不可恢复的错误：{e}")
+        exit(1)
--- src/util/config.py
+++ src/util/config.py
@@ -1,29 +1,87 @@
+import os
 import pathlib
 import json
+from typing import Any, Self
+
+import pydantic
 
 from src.util.log import log
 
 
 DEFAULT_CONFIG_PATH = pathlib.Path.cwd() / "config" / "config.json"
 CONFIG_API_VERSION = 1
+CONFIG_TEMPLATE = {
+    "api_version": CONFIG_API_VERSION,
+    "app": {
+        "detect_path": str(pathlib.Path.expanduser(pathlib.Path("~/Desktop"))),
+    },
+}
+
+
+class _AppConfig(pydantic.BaseModel):
+    detect_path: str
+
+    @pydantic.field_validator("detect_path")
+    @classmethod
+    def validate_path(cls, value: Any) -> bool:
+        try:
+            path = pathlib.Path(value)
+        except ValueError as e:
+            log.error(f"配置文件中指定的检测路径 ({value}) 不是一个有效的路径。")
+            raise ValueError(f"配置文件中指定的检测路径 ({value}) 不是一个有效的路径。", ) from e
+        else:
+            if not path.exists():
+                log.error(f"配置文件中指定的检测路径 ({value}) 不存在。")
+                raise FileNotFoundError(f"配置文件中指定的检测路径 ({value}) 不存在。")
+            return value
+
+
+class Config(pydantic.BaseModel):
+    api_version: int
+    app: _AppConfig
+
+    @pydantic.field_validator("api_version")
+    @classmethod
+    def validate_api_version(cls, value: Any) -> bool:
+        try:
+            ver = int(value)
+        except ValueError:
+            log.error(f"配置文件中指定的API版本 ({value}) 不是一个有效的整数。")
+            return False
+        else:
+            if ver != CONFIG_API_VERSION:
+                log.error(f"配置文件的API版本 ({ver}) 与当前版本 ({CONFIG_API_VERSION}) 不匹配。")
+                return False
+            return True
+
+
+def get_config(path: pathlib.Path = DEFAULT_CONFIG_PATH) -> Config:
+    """获取配置对象。"""
+    try:
+        return build_config(read_config_from(path))
+    except FileNotFoundError:
+        log.warning(f"配置文件 {path.absolute()!r} 不存在。正在初始化配置。")
+        os.makedirs(path.parent, exist_ok=True)
+        with path.open("w", encoding="utf8") as f:
+            json.dump(CONFIG_TEMPLATE, f, ensure_ascii=False, indent=4)
+        return build_config(CONFIG_TEMPLATE)
 
 
 def is_valid_config(config: dict) -> bool:
     """检查 ``config`` 是否是一个有效的配置字典。"""
-    api_version = config.get("api-version", 0)
-    if api_version != CONFIG_API_VERSION:
-        log.error(f"配置文件的API版本 ({api_version}) 与当前版本 ({CONFIG_API_VERSION}) 不匹配。")
-        return False
-    if "app" not in config.keys():
-        log.error("配置文件中缺少 'app' 键。")
-        return False
-    if "detect_path" not in config["app"].keys():
-        log.error("配置文件中缺少 'app.detect_path' 键。")
-        return False
-    if not pathlib.Path(config["app"]["detect_path"]).exists():
-        log.error(f"配置文件中指定的检测路径 ({config['app']['detect_path']}) 不存在。")
+    try:
+        build_config(config)
+    except (pydantic.ValidationError, ValueError) as e:
+        log.error(f"配置文件 {config} 不是一个有效的配置字典：{e}")
         return False
     return True
+
+
+def build_config(config: dict) -> Config:
+    """从 ``config`` 构建一个 ``Config`` 对象。"""
+    built = Config(**config)
+    log.debug(f"成功构建配置对象：{built}")
+    return built
 
 
 def read_config_from(path: pathlib.Path = DEFAULT_CONFIG_PATH) -> dict:
@@ -35,7 +93,7 @@ def read_config_from(path: pathlib.Path = DEFAULT_CONFIG_PATH) -> dict:
             return config
     except FileNotFoundError:
         log.error(f"配置文件 {path.absolute()!r} 不存在。")
-        return {}
+        raise
     except json.JSONDecodeError:
         log.error(f"配置文件 {path.absolute()!r} 的JSON格式错误。")
-        return {}
+        raise
