5c505434fd853cfb2ca8c91327055473496ad471
Macros Meng <Meng-yc@outlook.com>
2026-03-15 15:49:39 +0800

    feat: 主要逻辑 P2
    Signed-off-by: Macros Meng <Meng-yc@outlook.com>

--- remessydesktop.py
+++ remessydesktop.py
@@ -1,3 +1,4 @@
+import os
 import time
 import traceback
 
@@ -13,6 +14,7 @@ class ReMessyDesktop:
     def __init__(self):
         log.info(f'ReMessyDesktop ({BUILD_CODE}) 正在初始化……')
         self.config = config.get_config()
+        self.desktop_list = self._get_current_desktop_files()
         log.add(
             './logs/RMD-LOG-{time}.log',
             level=self.config.app.log_level,
@@ -37,7 +39,32 @@ class ReMessyDesktop:
             time.sleep(0)  # Take a break
 
     def on_check(self):
+        """
+        当一个 Watcher 触发检测时调用的方法。
+        """
         log.info('触发了分类!')
+        new_files = self._new_files()
+        log.debug(f'新增的文件：{new_files}')
+
+    def _get_current_desktop_files(self):
+        """
+        获取当前桌面文件夹中的所有文件（不包括文件夹）。
+        """
+        return [
+            f
+            for f in os.listdir(self.config.app.detect_path)
+            if not os.path.isdir(os.path.join(self.config.app.detect_path, f))
+        ]
+
+    def _new_files(self):
+        """
+        比较现在桌面和前一次更新 ``self.desktop_list`` 时的文件列表，返回新增的文件，更新 ``self.desktop_list``。
+        """
+        current_files = self._get_current_desktop_files()
+        new_files = list(set(current_files) - set(self.desktop_list))
+
+        self.desktop_list = current_files
+        return new_files
 
 
 if __name__ == '__main__':
